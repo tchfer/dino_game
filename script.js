d7dab96cc6ca7b6bda8462191c906ffee7807d5e
Flavio fernando da Silva <tchfernando@gmail.com>
2020-09-07 01:09:08 -0300

    Cactus created and moving left function working

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        jump();
+        if (!isJumping) {
+            jump();
+        }
     }
 }
 
 function jump() {
     let position = 0;
+    isJumping = true;
 
     let upInterval = setInterval(() => {
         if (position >= 150) {
@@ -17,6 +23,7 @@ function jump() {
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                 position -= 20;
                 dino.style.bottom = position + 'px';
@@ -30,4 +37,20 @@ function jump() {
     }, 20);
 }
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    // Cactus speed to make it more difficult
+    let leftInterval = setInterval(() => {
+        cactusPosition -= 10;
+        cactus.style.left = cactusPosition + 'px';
+    }, 20)
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp)
