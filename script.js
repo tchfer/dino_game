257b33c45cd622d9f019136afe5b47d162058227
Flavio fernando da Silva <tchfernando@gmail.com>
2020-09-07 00:50:51 -0300

    Dino created and jump function working

---
+++ script.js
@@ -0,0 +1,33 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        jump();
+    }
+}
+
+function jump() {
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval)
+
+            // Go down
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                } else {
+                position -= 20;
+                dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            // Go up        
+        position += 20
+        dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+document.addEventListener('keyup', handleKeyUp)
