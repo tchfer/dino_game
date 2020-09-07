30189c55f0f8caf15976daa4c8a134de9a2757f6
Flavio fernando da Silva <tchfernando@gmail.com>
2020-09-07 02:01:19 -0300

    Score added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
 let position = 0;
+let consecutiveJumps = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -50,10 +51,11 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+            consecutiveJumps++;
         } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
             // game over
             clearInterval(leftInterval);
-            document.body.innerHTML = '<h1 class="game-over">Game Over</h1>';
+            document.body.innerHTML = '<h1 class="game-over">Game Over. Your final score is: ' + consecutiveJumps + '.</h1>';
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
