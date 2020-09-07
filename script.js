058709de337ca7aa528a1118dc31fa2d14823c4a
Flavio fernando da Silva <tchfernando@gmail.com>
2020-09-07 01:38:00 -0300

    All game functions created

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
-
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -12,7 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -51,6 +50,10 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            // game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Game Over</h1>';
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
